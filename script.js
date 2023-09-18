1091431f244069512044a5fab4efd3a5057f7cfa
Timothy Meleki <muyiwameleki@gmail.com>
2023-09-19 00:17:19 +0100

    updates

--- script.js
+++ script.js
@@ -1,46 +1,62 @@
 let btn = document.getElementById("TogglePassword");
 
-        btn.addEventListener("click", toggle);
-        function toggle() {
-            if (inputedPassword.type === "password") {
-                inputedPassword.type = "text";
-            } else {
-                inputedPassword.type = "password";
-            }
-        }
-        let inputedPassword = document.getElementById("inputedPassword");
-        let displayRes = document.getElementById("displayRes");
-        let CheckPassword = document.getElementById("CheckPassword");
-        CheckPassword.addEventListener("click", checker);
+btn.addEventListener("click", toggle);
 
-        function checker() {
-            let patternMain = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*\W).{8,}$/;
-            let pattern1 = /[A-Z]+/;
-            let pattern2 = /[a-z]+/;
-            let pattern3 = /[0-9]+/;
-            let pattern4 = /\w+/;
-            let pattern5 = /\W+/;
-            let pattern6 = /\w+\W+/;
+function toggle() {
+    if (inputedPassword.type === "password") {
+        inputedPassword.type = "text";
+    } else {
+        inputedPassword.type = "password";
+    }
+}
 
-            let passwordExtract = inputedPassword.value;
+let inputedPassword = document.getElementById("inputedPassword");
+let displayRes = document.getElementById("displayRes");
+let CheckPassword = document.getElementById("CheckPassword");
 
-            if (passwordExtract.length < 8) {
-                displayRes.innerHTML = "Enter a password that is 8 characters long at least";
-            } else if (!patternMain.test(passwordExtract)) {
-                if (pattern1.test(passwordExtract)) {
-                    displayRes.innerHTML = "A strong password needs more than just uppercase characters.";
-                } else if (pattern2.test(passwordExtract)) {
-                    displayRes.innerHTML = "A strong password needs more than just lowercase characters.";
-                } else if (pattern3.test(passwordExtract)) {
-                    displayRes.innerHTML = "A strong password needs more than just numeric characters.";
-                } else if (pattern4.test(passwordExtract)) {
-                    displayRes.innerHTML = "A strong password needs special characters.";
-                } else if (pattern5.test(passwordExtract)) {
-                    displayRes.innerHTML = "A strong password needs more than just special characters.";
-                } else if (pattern6.test(passwordExtract)) {
-                    displayRes.innerHTML = "Not strong enough.";
-                }
-            } else {
-                displayRes.innerHTML = `You have inputted a very strong password of <b>${passwordExtract.length}</b> characters long`;
-            }
-        }
+CheckPassword.addEventListener("click", checker);
+
+function checker() {
+    let passwordExtract = inputedPassword.value;
+    let strength = 0;
+    let pattern = /[A-Z]/;
+    let pattern1 = /[a-z]/;
+    let pattern2 = /\d/;
+    let pattern3 = /\W/;
+
+    if (passwordExtract.length < 8) {
+        displayRes.innerHTML = "Enter at least 8 characters boss, no dey whine me";
+        return;
+    }
+
+    if (pattern.test(passwordExtract)) {
+        strength++;
+    }
+    if (pattern1.test(passwordExtract)) {
+        strength++;
+    }
+    if (pattern2.test(passwordExtract)) {
+        strength++;
+    }
+    if (pattern3.test(passwordExtract)) {
+        strength++;
+    }
+
+    // Determine the password strength based on the strength counter
+    switch (strength) {
+        case 1:
+            displayRes.innerHTML = "25% Na password be this? Dey play.";
+            break;
+        case 2:
+            displayRes.innerHTML = "50% My Grandma can do better";
+            break;
+        case 3:
+            displayRes.innerHTML = "75% Not Bad, you try well well";
+            break;
+        case 4:
+            displayRes.innerHTML = "100% Senior man, you sure say you go fit remember this password so?";
+            break;
+        default:
+            displayRes.innerHTML = "Shey you dey whine me";
+    }
+}
